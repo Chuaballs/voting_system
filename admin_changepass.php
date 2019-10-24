869d9d0af808219bb7ad9ec95839b289c5eb3e25
Duane Correa <correa.duanearvin@gmail.com>
2019-10-25 02:40:17 +0800

    duane-backend-codes

--- admin_changepass.php
+++ admin_changepass.php
@@ -1,16 +1,56 @@
+<?php
+session_start();
+require_once $_SERVER['DOCUMENT_ROOT'].'/voting_system/resource/php/class/function/checkaccount.php';
+$checkaccount = new checkaccount();
+$checkaccount ->  checkadmin();
+if (isset($_POST['change'])) {
+  require_once $_SERVER['DOCUMENT_ROOT'].'/voting_system/resource/php/class/function/changepass.php';
+$changepassword = new changepass($_POST['confirmpass'],$_POST['newpass']);
+$changepassword ->changepassword();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Registrar Portal</title>
+  <title>Voting System</title>
   <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap.min.css">
   <link href="vendor/css/all.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap-select.min.css">
 
 </head>
 <body>
+  <form method="POST">
 
+            <div class="form-group row">
+             <label for="password" class="col-md-4 col-form-label text-md-right">Old Password</label>
+              <div class="col-md-7">
+                <input id="password" type="password" class="form-control " name="confirmpass" required autocomplete="new-password">
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <label for="password-confirm" class="col-md-4 col-form-label text-md-right">New Password</label>
+                <div class="col-md-7">
+                  <input id="password-confirm" type="password" class="form-control" name="newpass" required autocomplete="new-password">
+                </div>
+              </div>
+
+            <div class="form-group row mb-0">
+              <div class="col-md-6 offset-md-5">
+                <a href="index.php">
+                  <button type="submit" name="change"class="btn btn-primary">
+                  Change
+                  </button>
+                  <a href="admin_homepage.php">
+                  Back
+                </a>
+                </a>
+              </div>
+           </div>
+
+        </form>
 </body>
     <script src="vendor/js/jquery.js"></script>
     <script src="vendor/js/popper.js"></script>
--- admin_homepage.php
+++ admin_homepage.php
@@ -1,17 +1,34 @@
+<?php
+session_start();
+require_once $_SERVER['DOCUMENT_ROOT'].'/voting_system/resource/php/class/function/checkaccount.php';
+$checkaccount = new checkaccount();
+$checkaccount ->  checkadmin();
+if (isset($_POST['logout'])) {
+  require_once $_SERVER['DOCUMENT_ROOT'].'/voting_system/resource/php/class/function/logout.php';
+$logout = new logout();
+$logout -> logouts();
+}
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Registrar Portal</title>
+  <title>Voting System</title>
   <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap.min.css">
   <link href="vendor/css/all.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap-select.min.css">
 
-</head>
-<body>
-
-</body>
+  </head>
+    <body>
+      <a href="admin_view_score">View Score</a></br>
+      <a href="admin_changepass">Change Password</a></br>
+      <a href="admin_register_voter.php">Add Voters</a></br>
+      <a href="admin_register_candidate.php">Add candidate</a>
+      <form action="" method="post">
+                <input type="submit" name="logout" value="logout">
+      </form>
+    </body>
     <script src="vendor/js/jquery.js"></script>
     <script src="vendor/js/popper.js"></script>
     <script src="vendor/js/bootstrap.min.js"></script>
