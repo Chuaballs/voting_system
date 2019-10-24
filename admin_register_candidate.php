ed9172645eb03849196ae7089c5290cae1c24300
Duane Correa <correa.duanearvin@gmail.com>
2019-10-24 17:39:38 +0800

    final starter kit

---
+++ admin_register_candidate.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registrar Portal</title>
+  <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap.min.css">
+  <link href="vendor/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap-select.min.css">
+
+</head>
+<body>
+
+</body>
+    <script src="vendor/js/jquery.js"></script>
+    <script src="vendor/js/popper.js"></script>
+    <script src="vendor/js/bootstrap.min.js"></script>
+    <script src="vendor/js/bootstrap-select.min.js"></script>
+</body>
+</html>
