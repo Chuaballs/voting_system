1c4b118c49e88d44a6f6503cd302c79fb7cb78c5
rcmramirez910@gmail.com <rcramirez910@gmail.com>
2019-10-25 16:06:11 +0800

    fixedtitle

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $login -> login();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Registrar Portal</title>
+  <title>VOTE!</title>
   <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap.min.css">
   <link href="resource/css/all.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap-select.min.css">
